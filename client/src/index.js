a3c1bd320336d74c2272b8627d7d4aaf48cc61b8
Hariharan Thiagarajan <hariharansmm@gmail.com>
2022-08-12 01:42:13 +1200

    Sixth Commit
    Update Reducers Index and Posts

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 root.render(
-    <React.StrictMode>
+    <React.StrictMode store={ store }>
         <App />
     </React.StrictMode>
 );
